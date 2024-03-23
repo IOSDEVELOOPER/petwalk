87dcc96c9182b47dfea72ddca1d9240cbcddf345
IOSDEVELOOPER <120388224+IOSDEVELOOPER@users.noreply.github.com>
2024-03-23 21:17:11 +0300

    Add files via upload

---
+++ PetWalk/PetWalk/PetWalkApp.swift
@@ -0,0 +1,17 @@
+//
+//  PetWalkApp.swift
+//  PetWalk
+//
+//  Created by Андрей Федоров on 23.03.2024.
+//
+
+import SwiftUI
+
+@main
+struct PetWalkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
